12b4486afec500c77540e4c891f8b034e5388015
krjohnn <ks91ko@gmail.com>
2025-11-16 00:29:06 +0200

    Added initial DB and DB import function.

--- MD1/md1.py
+++ MD1/md1.py
@@ -5,6 +5,7 @@ import redis
 from typing import List, Dict, Any
 import json
 import os
+import shlex
 from dotenv import load_dotenv
 
 # ============================================
@@ -19,6 +20,10 @@ def connect_to_redis():
     redis_port = os.getenv("REDIS_PORT")
     redis_user = os.getenv("REDIS_USER")
     redis_password = os.getenv("REDIS_PASSWORD")
+
+    if not all([redis_host, redis_port, redis_user, redis_password]):
+        print("Error: Required environment variables are not set.")
+        return None
 
     r = redis.Redis(
         host=redis_host,
@@ -35,17 +40,53 @@ def connect_to_redis():
         print("Could not connect to Redis")
         return None
 
+
+def run_cli_script_seq(r, filename="data.redis"):
+    if not r:
+        return
+
+    print(f"Opening script file: {filename}")
+    command_count = 0
+    try:
+        with open(filename, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+
+                try:
+                    parts = shlex.split(line)
+                    r.execute_command(*parts)
+                    command_count += 1
+
+                except redis.exceptions.RedisError as e:
+                    print(f"Error running command: {line}")
+                    print(f"Redis error: {e}")
+
+        print(f"\nSuccessfully executed {command_count} commands one by one.")
+
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def main():
     """Run sample operations and reports"""
     r = connect_to_redis()
     if not r:
         return
 
-    print("TEST AREA BEGINS --------------------")
-    print("TEST AREA ENDS --------------------")
+    run_cli_script_seq(r, filename="create_movie_db.redis")
+
+    # Add 100 entries - Done
+    # Edit 50 entires - To Do
+    # Select 50 entries - To Do
+    # Delete 50 entries - To Do
+
+
+    r.close()
     return
 
 if __name__ == "__main__":
     main()
-
-# r.close()
