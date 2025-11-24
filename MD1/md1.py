05e379cae611d1f6aba0b922e070608e6a32e9cc
krjohnn <ks91ko@gmail.com>
2025-11-24 03:24:44 +0200

    Updated deletion logic. Added order clause for select functions. Added selections by rating and genre.

--- MD1/md1.py
+++ MD1/md1.py
@@ -92,15 +92,15 @@ def update_movie_data(r: redis.Redis, json_data: List[Dict[str, Any]]) -> None:
 def delete_movie_data(r: redis.Redis, json_data: List[Dict[str, Any]]) -> None:
     for movie in json_data.get("movies", []):
         movie_id = movie.get("id")
-        r.delete(f"movie:{movie_id}")
         r.zrem("boxoffice:revenue", movie_id)
         r.zrem("top:rated", movie_id)
-        if 'genre' in movie:
-            r.srem(f"genre:{movie['genre']}", movie_id)
+        genre = r.hget(f"movie:{movie_id}", "genre")
+        r.srem(f"genre:{genre}", movie_id)
+        r.delete(f"movie:{movie_id}")
     print(f"Deleted {len(json_data.get('movies', []))} movies from Redis.")
 
-def select_movie_data_by_name(r: redis.Redis, movie_name: List) -> None:
-    movie_id = r.keys(f"movie:*")
+def select_movie_data_by_name(r: redis.Redis, movie_name: List[str]) -> None:
+    movie_id = r.scan_iter(f"movie:*")
     for mid in movie_id:
         movie = r.hgetall(mid)
         if movie.get("title") in movie_name:
@@ -108,12 +108,29 @@ def select_movie_data_by_name(r: redis.Redis, movie_name: List) -> None:
             for(key, value) in movie.items():
                 print(f"{key}: {value}")
 
-def select_top_n_movies_by_revenue(r: redis.Redis, n: int) -> None:
-    top_movies = r.zrevrange("boxoffice:revenue", 0, n-1, withscores=True)
+def select_top_n_movies_by_revenue(r: redis.Redis, order: str , n: int) -> None:
+    if order == "top":
+        top_movies = r.zrevrange("boxoffice:revenue", 0, n-1, withscores=True)
+    if order == "bottom":
+        top_movies = r.zrange("boxoffice:revenue", 0, n-1, withscores=True)
     print(f"\nTop {n} Movies by Revenue:")
     for movie_id, revenue in top_movies:
         movie = r.hgetall(f"movie:{movie_id}")
         print(f"Title: {movie.get('title')}, Revenue: {revenue}")
+
+def select_top_n_movies_by_rating(r: redis.Redis, n: int) -> None:
+    top_movies = r.zrevrange("top:rated", 0, n-1, withscores=True)
+    print(f"\nTop {n} Movies by Rating:")
+    for movie_id, rating in top_movies:
+        movie = r.hgetall(f"movie:{movie_id}")
+        print(f"Title: {movie.get('title')}, Rating: {rating}")
+
+def select_movies_by_genre(r: redis.Redis, genre: str) -> None:
+    movie_ids = r.smembers(f"genre:{genre}")
+    print(f"\nMovies in Genre '{genre}':")
+    for movie_id in movie_ids:
+        movie = r.hgetall(f"movie:{movie_id}")
+        print(f"Title: {movie.get('title')}")
 
 def main():
     r = connect_to_redis()
@@ -125,24 +142,25 @@ def main():
 
         # Add 100 entries - Done
         json_data = load_json(filename="in_import_data.json")
-
         import_movie_data(r, json_data)
         import_director_data(r, json_data)
         import_actor_data(r, json_data)
 
         # Edit 50 entires - To D
         json_data = load_json(filename="in_update_data.json")
-
         update_movie_data(r, json_data)
 
         # Select 50 entries - To Do
         select_movie_data_by_name(r, movie_name=["Pulp Fiction", "Inception", "Interstellar", "The Dark Knight", "Forrest Gump", "The Matrix", "The Shawshank Redemption", "The Godfather", "The Lord of the Rings: The Return of the King", "Fight Club"])
-        select_top_n_movies_by_revenue(r, 10)
+        select_top_n_movies_by_revenue(r, "top",10)
+        select_top_n_movies_by_revenue(r, "bottom", 10)
+        select_movies_by_genre(r, "Sci-Fi")
 
         # Delete 50 entries - To Do
         json_data = load_json(filename="in_delete_data.json")
-
         delete_movie_data(r, json_data)
+
+        select_movies_by_genre(r, "Sci-Fi")
 
     else:
         print(f"Redis database is not empty: {r.dbsize()} entries found.")
