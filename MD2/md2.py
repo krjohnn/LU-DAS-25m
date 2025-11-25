c9fb133a56eeb308b2feff3ccf8f3c3281aa2566
krjohnn <ks91ko@gmail.com>
2025-11-26 01:29:35 +0200

    Added insurance company and person data imports

--- MD2/md2.py
+++ MD2/md2.py
@@ -39,23 +39,56 @@ def load_json(filename="data.json"):
         print(f"Error decoding JSON from file {filename}.")
         raise
 
+def delete_all_nodes(n):
+    try:
+        with n.session() as session:
+            session.run("MATCH (n) DETACH DELETE n")
+            print("All nodes and relationships deleted successfully.")
+    except Exception as e:
+        print(f"Error deleting nodes: {e}")
+        raise
+
+def import_insurance_company_data(n, json_data: List[Dict[str, Any]]) -> None:
+    for insurance_company in json_data.get("insurance_companies", []):
+        n.execute_query("""
+        CREATE (c:InsuranceCompany {id: $id, name: $name, address: $address, contact_email: $contact_email})
+        """, id=insurance_company.get("id"), name=insurance_company.get("name"), address=insurance_company.get("address"),
+                        contact_email=insurance_company.get("contact_email"), database_="neo4j")
+    print(f"Imported {len(json_data.get("insurance_companies", []))} insurance companies into Neo4j.")
+
+
+def import_person_data(n, json_data: List[Dict[str, Any]]) -> None:
+    for person in json_data.get("persons", []):
+        n.execute_query("""
+        CREATE (p:Person {social_security_number: $social_security_number, full_name: $full_name, date_of_birth: $date_of_birth, address: $address, phone_number: $phone_number, risk_level: $risk_level})
+        """, social_security_number=person.get("social_security_number"), full_name=person.get("full_name"),
+                        date_of_birth=person.get("date_of_birth"), address=person.get("address"),
+                        phone_number=person.get("phone_number"), risk_level=person.get("risk_level"), database_="neo4j")
+    print(f"Imported {len(json_data.get("persons", []))} persons into Neo4j.")
+
 def main():
     n = connect_to_neo4j()
     if not n:
         return
 
-    with n.session() as session:
-        summary = n.execute_query("""
-            CREATE (a:Person {name: $name})
-            CREATE (b:Person {name: $friendName})
-            CREATE (a)-[:KNOWS]->(b)
-            """, name="Alice", friendName="David", database_="neo4j", ).summary
-        print("Created {nodes_created} nodes in {time} ms.".format(
-            nodes_created=summary.counters.nodes_created,
-            time=summary.result_available_after
-        ))
+    # insurance_companies -- DONE
+    # persons -- DONE
+    # policies
+    # cars
+    # accidents
+    # claims
 
-        n.close()
+    json_data = load_json(filename="in_import_data.json")
+
+    for i in json_data.get("persons", []):
+        print(i)
+
+    import_person_data(n, json_data)
+
+    #import_insurance_company_data(n, json_data)
+
+    #delete_all_nodes(n)
+    n.close()
 
     return
 
