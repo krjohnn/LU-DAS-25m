2c15ea6f12e85debde9ae1ed45c5c25109e11f38
krjohnn <ks91ko@gmail.com>
2025-12-03 01:20:47 +0200

    Added connection logic.

--- MD3/md3.py
+++ MD3/md3.py
@@ -2,9 +2,10 @@ import os
 import json
 import logging
 from dotenv import load_dotenv
-from pymongo.mongo_client import MongoClient
+from pymongo.mongo_client import MongoClient, InsertOne
 from pymongo.server_api import ServerApi
 from pymongo.errors import ConnectionFailure, ConfigurationError
+from requests import session
 
 
 def connect_to_mongodb():
@@ -55,22 +56,37 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     m = connect_to_mongodb()
+    if not m:
+        return
 
-    print(m.admin.command('ping'))
-    m.close()
+    perform_import = False
 
-    try:
-        print(m.admin.command('ping'))
-    except (ConnectionFailure, ConfigurationError) as e:
-        logging.error(f"MongoDB Connection failed: {e}")
-        return None
-    except Exception as e:
-        logging.error(f"An unexpected error occurred: {e}")
-        return None
+    json_data = load_json(filename="stations.json")
+
+    print(m.get_database("sample_mflix"))
+    db = m["EV_Monitoring"]
+    collection = db["charging_stations"]
+    requesting = []
+
+    for item in json_data:
+        requesting.append(InsertOne(item))
+
+    result = collection.bulk_write(requesting)
+    print(result)
+
+    print(m.list_database_names())
 
 
+    # collection_list = database.list_collections()
+    # for c in collection_list:
+    #     print(c)
+
+
+
+    logging.info("\n" + "=" * 40+"\n      STARTING IMPORT PROCESS" + "\n" + "=" * 40)
     # do things
 
+    m.close()
     return
 
 if __name__ == "__main__":
